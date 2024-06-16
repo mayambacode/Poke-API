2811ccb5c7f428b6f8b6278f4739a80ab81b0d21
mayambacode <bokambma@sheridancollege.ca>
2024-06-15 21:54:43 -0400

    Poke API

---
+++ index.js
@@ -0,0 +1,44 @@
+
+/*
+    fetch = Function used for making HTTP requests to fetch resources.
+            (JSON style data, images, files)
+            Simplifies asynchonous data fetching in JavaScript and 
+            used for interacting with APIs to retrieve and send 
+            data asynchonously over the web.
+            fetch(url, {options}) 
+*/
+
+// fetch("https://pokeapi.co/api/v2/pokemon/pikachu")
+// .then(response => {
+
+//     if (!response.ok) {
+//         throw new Error("Can not fetch resource");
+//     }
+//     return response.json();
+// })
+// .then(data => console.log(data))
+// .catch(error => console.log(error));
+
+fetchData();
+
+async function fetchData() {
+
+    try {
+
+        const pokemonName = document.getElementById("pokemonName").value.toLowerCase();
+        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`)
+
+        if(!response.ok) {
+            throw new Error("Could not fetch resource");
+        }
+        const data = await response.json();
+        const pokemonSprite = data.sprites.front_default;
+        const imgElement = document.getElementById("pokemonSprite");
+
+        imgElement.src = pokemonSprite;
+        imgElement.style.display = "block";
+    }
+    catch(error) {
+        console.log(error);
+    }
+}
